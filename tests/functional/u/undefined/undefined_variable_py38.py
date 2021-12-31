60916f9a861610a72606e54f33d86ed408b92868
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-12-31 14:25:01 +0100

    Better capitalization in docstring, fixed number of char per line

--- tests/functional/u/undefined/undefined_variable_py38.py
+++ tests/functional/u/undefined/undefined_variable_py38.py
@@ -1,11 +1,11 @@
-"""Tests for undefined variable with assignment expressions"""
+"""Tests for undefined variable with assignment expressions."""
 # pylint: disable=using-constant-test, expression-not-assigned
 
 # Tests for annotation of variables and potentially undefinition
 
 
 def typing_and_assignment_expression():
-    """The variable gets assigned in an assignment expression"""
+    """The variable gets assigned in an assignment expression."""
     var: int
     if (var := 1 ** 2):
         print(var)
@@ -19,7 +19,7 @@ def typing_and_self_referencing_assignment_expression():
 
 
 def typed_assignment_in_function_default(param: str = (typed_default := "walrus")) -> None:
-    """An annotated assignment expression in a default parameter should not emit"""
+    """An annotated assignment expression in a default parameter should not emit."""
     return param
 
 
@@ -27,7 +27,7 @@ print(typed_default)
 
 
 def assignment_in_function_default(param = (default := "walrus")) -> None:
-    """An assignment expression in a default parameter should not emit"""
+    """An assignment expression in a default parameter should not emit."""
     return param
 
 
@@ -35,7 +35,7 @@ print(default)
 
 
 def no_assignment_in_function_default(param: str = "walrus") -> None:
-    """No annotated assignment expression should emit"""
+    """No annotated assignment expression should emit."""
     return param
 
 
@@ -43,7 +43,7 @@ print(no_default) # [undefined-variable]
 
 
 def no_parameters_in_function_default() -> None:
-    """Regression tests for functions without any parameters"""
+    """Regression tests for functions without any parameters."""
     return
 
 
